ff9406eb1eafc70409a0a91cb79e6fd601bca8b8
vishwasprabhu98 <vishwasprabhu8@gmail.com>
2025-04-01 18:20:29 +0530

    fix: added baseUrl as https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://api.nobelprize.org/2.1'
+  baseUrl: 'https://api.nobelprize.org/2.1',
+  // baseUrl: 'http://api.nobelprize.org/2.1', // unsecure => throws error in firebase
+
+  // Please uncomment below url (http://localhost:3000) line incase of the CORS error from http://api.nobelprize.org/2.1
+  // and execute command `npm run server` in terminal.
   // baseUrl: 'http://localhost:3000'
 }
